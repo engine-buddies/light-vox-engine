62511634f63d89719850d4af5978ab631ada01a1
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-10-14 18:20:41 -0400

    Add JobPriority enums and args member variables to CpuJob

--- Light Vox Engine/CpuJob.h
+++ Light Vox Engine/CpuJob.h
@@ -1,5 +1,16 @@
 #pragma once
 
+/// <summary>
+/// Priority of a CPU job. Will determine how the job 
+/// is sorted into the job queue
+/// </summary>
+enum JobPriority
+{
+    CRITICAL,
+    HIGH,
+    NORMAL,
+    LOW
+};
 
 /// <summary>
 /// A job that will be run in parallel with others on the CPU
@@ -8,8 +19,13 @@
 /// <author>Ben Hoffman</author>
 struct CpuJob
 {
-    int priority;
-    void( *func_ptr )( void* arg, int index );
+    JobPriority priority = JobPriority::NORMAL;
+
+    void( *func_ptr )( void* arg, int index ) = nullptr;
+    
+    void* args = nullptr;
+    
+    int index = 0;
 };
 
 // This is the definition of a CPU job that we have to roll with
