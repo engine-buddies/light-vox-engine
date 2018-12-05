15079ff6c4a22de0285641684215a31c6d282c9a
Ben Hoffman <bah8892@g.rit.edu>
2018-12-05 02:46:16 -0500

    Fix issue with the components being stack-allocated (#57)
    * Move all entity components to the heap
    
    * Format spacing in files

--- Light Vox Engine/ECS/ComponentManager.h
+++ Light Vox Engine/ECS/ComponentManager.h
@@ -7,17 +7,18 @@ namespace ECS {
     {
     public:
         //SOA components
-        EntityComponents::TransformMatrix transformMatrix[ LV_MAX_INSTANCE_COUNT ];
-        EntityComponents::Transform transform[LV_MAX_INSTANCE_COUNT];
-        EntityComponents::BoxCollider boxCollider[LV_MAX_INSTANCE_COUNT];
-        EntityComponents::BodyProperties bodyProperties[LV_MAX_INSTANCE_COUNT];
-        EntityComponents::Contacts contacts[LV_MAX_INSTANCE_COUNT];
+        EntityComponents::TransformMatrix* transformMatrix;
+        EntityComponents::Transform* transform;
+        EntityComponents::BoxCollider* boxCollider;
+        EntityComponents::BodyProperties* bodyProperties;
+        EntityComponents::Contacts* contacts;
+
         static ComponentManager* GetInstance();
         void Init();
         void ReleaseInstance();
 
-        ComponentManager(ComponentManager const&) = delete;
-        void operator=(ComponentManager const&) = delete;
+        ComponentManager( ComponentManager const& ) = delete;
+        void operator=( ComponentManager const& ) = delete;
 
     private:
         ComponentManager();
